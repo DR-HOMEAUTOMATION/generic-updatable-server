08cc6ea407f2007b152a4af4398c69fb76c82b8d
dawson <dawsonreschke@icloud.com>
2022-04-17 19:54:41 -0700

    added libcam-vid functionality, testing pre-push

--- src/servers/ImageServer.js
+++ src/servers/ImageServer.js
@@ -2,24 +2,42 @@ const config = require('../../config')
 const fs = require('fs')
 const express = require('express')
 const router = express.Router(); 
-const { JpegCam }= require('libcamera-js')
+const {VidCam} = require('libcamera-js')
 
-const successImageCapture = (res,code) =>{
-    res.json({data:fs.readFileSync(`${process.cwd()}/${config.jpeg_cam_config['-o']}`)})
-    console.log(code)
-}
+let [getImage,exit] = new VidCam().createSegmentedMJPEGVideoStream({'-o':'test.jpeg','-t':'0'})
 
-const failedImageCapture = (res,err) =>{
-    console.log(err)
-}
+router.get('/image',(req,res)=>{
+    res.json({data:getImage()})
+})
 
-try{
-    const jpegCam = new JpegCam(config.jpeg_cam_config)
-    router.get('/image',(req,res)=>{
-        jpegCam.getNewImage((code)=>successImageCapture(res,code))
-    })
-}catch(err){
-    console.log(err)
-}
+router.get('/stop',(req,res)=>{
+    exit()
+    res.json({message:'stopping the video stream'})
+})
 
-module.exports = router
+router.get('/start',(req,res)=>{
+    res.json({message:'starting the video stream'})
+    [getImage,exit] = new VidCam().createSegmentedMJPEGVideoStream({'-o':'test.jpeg','-t':'0'})
+})
+
+// const { JpegCam }= require('libcamera-js')
+
+// const successImageCapture = (res,code) =>{
+//     res.json({data:fs.readFileSync(`${process.cwd()}/${config.jpeg_cam_config['-o']}`)})
+//     console.log(code)
+// }
+
+// const failedImageCapture = (res,err) =>{
+//     console.log(err)
+// }
+
+// try{
+//     const jpegCam = new JpegCam(config.jpeg_cam_config)
+//     router.get('/image',(req,res)=>{
+//         jpegCam.getNewImage((code)=>successImageCapture(res,code))
+//     })
+// }catch(err){
+//     console.log(err)
+// }
+
+// module.exports = router
