6347ce5cdb4e649e0ea0629584251dbdc91cbeca
dawson <dawsonreschke@icloud.com>
2022-04-21 01:21:28 -0700

    .

--- libcamera/lib/libcamera_vid/index.js
+++ libcamera/lib/libcamera_vid/index.js
@@ -21,7 +21,8 @@ class VidCam{
         this.vid.on('error',errorCB || Function.prototype)
         this.vid.on('data',(data)=>console.log('\x1b[33m',`data: ${data}`,'\x1b[0m'))
         return[
-            (res)=>fs.createReadStream(`${process.cwd()}/${args['-o']}`).pipe(res), // [0] = getImg
+            async (res) => res.send(fs.readFile(`${process.cwd()}/${args['-o']}`)),
+            // (res)=>fs.createReadStream(`${process.cwd()}/${args['-o']}`).pipe(res), // [0] = getImg
             (code,onClosed)=>{
                 this.vid.on('close',onClosed || Function.prototype)
                 this.vid.emit('exit',code || 1)
